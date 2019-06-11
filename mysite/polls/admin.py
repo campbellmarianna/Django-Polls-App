97ac290e955fc3829dbbebe3621004a3bbd647b3
Marianna <mckd2018@gmail.com>
2019-06-11 14:20:49 -0700

    [add] fieldsets

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from .models import Question
 
 class QuesitonAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Data information', {'fields': ['pub_date']}),
+    ]
 
 admin.site.register(Question, QuesitonAdmin)
