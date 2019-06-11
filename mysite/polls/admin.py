1570eb35153d356351b968e59c23403846a4c9df
Marianna <mckd2018@gmail.com>
2019-06-11 14:54:34 -0700

    [add] choices to the question

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class QuesitonAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Data information', {'fields': ['pub_date']}),
+        ('Data information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuesitonAdmin)
